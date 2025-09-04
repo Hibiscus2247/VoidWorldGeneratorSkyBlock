3ac848530a867c0a8c05ad7c2fc1af736e976a2e
Hibiscus2247 <hibiscusmistre1@gmail.com>
2025-09-04 11:09:55 -0500

    can now place blocks yay!

--- src/main/java/fr/formiko/mc/voidworldgenerator/VoidWorldGeneratorPlugin.java
+++ src/main/java/fr/formiko/mc/voidworldgenerator/VoidWorldGeneratorPlugin.java
@@ -40,32 +40,48 @@ public class VoidWorldGeneratorPlugin extends JavaPlugin {
 
         @Override
         public void generateNoise(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ,
-                @NotNull ChunkData chunkData) {
-            // No need to generate noise, we want an empty world
+                                  @NotNull ChunkData chunkData) {
+            // Initialize chunk bounds by accessing the chunk data methods
+            // This establishes proper world boundaries without placing blocks
+            chunkData.getMinHeight();
+            chunkData.getMaxHeight();
         }
+
         @Override
         public void generateSurface(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ,
-                @NotNull ChunkData chunkData) {
+                                    @NotNull ChunkData chunkData) {
             // No need to generate surface, we want an empty world
         }
         @Override
         public void generateBedrock(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ,
-                @NotNull ChunkData chunkData) {
+                                    @NotNull ChunkData chunkData) {
             // No need to generate bedrock, we want an empty world
         }
         @Override
         public void generateCaves(@NotNull WorldInfo worldInfo, @NotNull Random random, int chunkX, int chunkZ,
-                @NotNull ChunkData chunkData) {
+                                  @NotNull ChunkData chunkData) {
             // No need to generate caves, we want an empty world
         }
 
         @Override
-        public boolean canSpawn(World world, int x, int z) { return true; }
+        public boolean canSpawn(World world, int x, int z) {
+            // Check if coordinates are within reasonable bounds
+            return Math.abs(x) < 30000000 && Math.abs(z) < 30000000;
+        }
 
         @Override
         public Location getFixedSpawnLocation(World world, Random random) {
-            return new Location(world, configSettings.getSpawnX(worldName), configSettings.getSpawnY(worldName),
+            // Ensure spawn Y is within world bounds
+            int spawnY = configSettings.getSpawnY(worldName);
+            if (spawnY < world.getMinHeight()) {
+                spawnY = world.getMinHeight() + 1;
+            } else if (spawnY >= world.getMaxHeight()) {
+                spawnY = world.getMaxHeight() - 1;
+            }
+
+            return new Location(world, configSettings.getSpawnX(worldName), spawnY,
                     configSettings.getSpawnZ(worldName));
         }
+
     }
 }
